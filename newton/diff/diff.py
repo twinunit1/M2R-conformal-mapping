2ed8fef4bb6ef56fe8c118c22dbd24d87a41b24b
Dat Duong <123020148+ddatba@users.noreply.github.com>
2023-05-31 13:51:07 +0100

    Update diff.py

--- newton/diff/diff.py
+++ newton/diff/diff.py
@@ -1,9 +1,7 @@
 import dual as d
 import numpy as np
+import matplotlib.pyplot as plt
 
-def diff1(f1, val):
-    x = f1(d.Dual(real=val, dual={'1': 1}))
-    return x.dual['1']
 
 def diff2(f1, f2, val):
     if len(val)==2:
@@ -15,21 +13,31 @@ def diff2(f1, f2, val):
     else:
         raise Exception('wrong dimension for val')
 
-def newt(f1, f2, F, err=1e-10, val=[0,0], n=100):
+def newt(f1, f2, f3, val, err=1e-10, n=100):
     #initialisation
     m = n
     df = diff2(f1, f2, val)
-    f = np.array([f1(*val), f2(*val)])-np.array(F)
+    f = np.array([-f1(*val), -f2(*val)]) + np.array(f3)
     delta = np.linalg.solve(df, f)
     nval = np.array(val)
     #loop
-    while 1 in [a>=err for a in delta] and m>0:
-        nval = nval - delta
+    while 1 in [abs(a) >= err for a in delta] and m > 0:
+        nval = nval + delta
         df = diff2(f1, f2, nval)
-        f = np.array([f1(*nval), f2(*nval)])-np.array(F)
+        f = np.array([-f1(*nval), -f2(*nval)]) + np.array(f3)
         delta = np.linalg.solve(df, f)
         m -= 1
-    if m>0:
-        return nval - delta
+    if m > 0:
+        return nval + delta
     else: 
         raise Exception('max iteration reached')
+        
+def ceff(f1, f2, maxd):
+    D = np.arange(1,1000,1)
+    pval = []
+
+    for i in range(len(D)):
+        val = [D[i],1/2]
+        f3 = [D[i], D[i]+1]
+        pval.append(-2*np.pi/np.log(newt(f1, f2, f3, val)[1]))
+    plt.plot(D,pval)
