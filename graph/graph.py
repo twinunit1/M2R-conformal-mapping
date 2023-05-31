07c0530ee9b71b4f6637bab06345f37201c335f8
Dat Duong <123020148+ddatba@users.noreply.github.com>
2023-05-31 22:25:15 +0100

    Update graph.py

--- graph/graph.py
+++ graph/graph.py
@@ -19,21 +19,3 @@ def splt(h, n=16, m = 500, xbound = [-2,2], ybound = [-2,2], figsize=[10,10]):
     plt.contour(xval, yval, Re, [0,1], colors=['blue', 'red'])
     plt.axis(xbound+ybound)
     plt.axis('scaled')
-    
-def splt2(h, cmax=10, n=150, m = 500, xbound = [-2,2], ybound = [-2,2], figsize=[10,10]):
-    x = np.linspace(xbound[0],xbound[1],m)
-    y = np.linspace(xbound[0],ybound[1],m)
-    xval, yval = np.meshgrid(x,y)
-    z = xval + yval * 1j
-    f = h(z)
-    Re = f.real
-    Im = f.imag
-    Im = Im.astype(float)
-    Im = np.where(Re<=1, Im, np.nan)
-    Im = np.where(Re>=0, Im, np.nan)
-
-    plt.figure(figsize=figsize)
-    plt.contour(xval, yval, Im, np.linspace(-cmax,cmax,n), colors='black', linestyles='solid')
-    plt.contour(xval, yval, Re, [0,1], colors=['blue', 'red'])
-    plt.axis(xbound+ybound)
-    plt.axis('scaled')
