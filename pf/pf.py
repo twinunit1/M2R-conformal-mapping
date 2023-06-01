69737308cffb3549169b0ebbcf67a033cb789377
Dat Duong <123020148+ddatba@users.noreply.github.com>
2023-06-01 11:34:46 +0100

    Add files via upload

---
+++ pf/pf.py
@@ -0,0 +1,32 @@
+import dual as d
+import numpy as np
+
+
+def C(p, eps=1e-20):
+    if 0<=p<1:
+        n=2
+        c = (1-p**2)**2
+        while p**(2*n)>=eps:
+            c *= (1-p**(2*n))**2
+            n +=1
+        return c
+    else:
+        raise Exception('invalid p')
+
+def P(z,p, eps=1e-20):
+    if isinstance(z, d.Dual):
+        Z = z.real
+    else:
+        Z = z
+    if 0<=p<1:
+        n=2
+        a = (1-z*p**2)*(1-(p**2)/z)
+        while np.abs((Z+1/Z)*p**(2*n)-p**(4*n))>=eps:
+            a *= (1-z*p**(2*n))*(1-(p**(2*n))/z)
+            n +=1
+        return (1-z)*a
+    else:
+        raise Exception('invalid p')
+
+def wA(z,a,p, eps=1e-20):
+    return -a*P(z/a,p,eps**2)/C(p, eps**2)
