2f406f8720f7818fb4663da1dca31de2b96ea1bf
Dat Duong <123020148+ddatba@users.noreply.github.com>
2023-06-01 01:10:37 +0100

    Update graph.py

--- graph/graph.py
+++ graph/graph.py
@@ -2,27 +2,27 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def splt(h, n=16, m = 500, sing=False, a=1, xbound = [-2,2], ybound = [-2,2], figsize=[10,10], ax = True):
-    x = np.linspace(xbound[0],xbound[1],m)
-    y = np.linspace(xbound[0],ybound[1],m)
-    xval, yval = np.meshgrid(x,y)
+def splt(h, n = 16, m = 500, sing = False, a = 1, xbound = [-2,2], ybound = [-2,2], figsize = 10, ax = True):
+    x = np.linspace(xbound[0], xbound[1], m)
+    y = np.linspace(xbound[0], ybound[1], m)
+    xval, yval = np.meshgrid(x, y)
     z = xval + yval * 1j
     f = h(z)
     Re = f.real
     Im = f.imag
     Im = Im.astype(float)
     
-    Im = np.where(Re<=1, Im, np.nan)
-    Im = np.where(Re>=0, Im, np.nan)
+    Im = np.where(Re <= 1, Im, np.nan)
+    Im = np.where(Re >= 0, Im, np.nan)
     
     if sing:
-        Im = np.where(Im<np.nanmax(Im)-10**(-a), Im, np.nan)
-        Im = np.where(Im>np.nanmin(Im)+10**(-a), Im, np.nan)
+        Im = np.where(Im < np.nanmax(Im) - 10 ** (-a), Im, np.nan)
+        Im = np.where(Im > np.nanmin(Im) + 10 ** (-a), Im, np.nan)
 
-    plt.figure(figsize=figsize)
-    plt.contour(xval, yval, Im, n-1, colors='black', linestyles='solid')
+    plt.figure(figsize = [figsize, figsize])
+    plt.contour(xval, yval, Im, n-1, colors = 'black', linestyles = 'solid')
     plt.contour(xval, yval, Re, [0,1], colors=['blue', 'red'])
     plt.axis('scaled')
-    plt.axis(xbound+ybound)
+    plt.axis(xbound + ybound)
     if not ax:
         plt.axis('off')
