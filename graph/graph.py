84d07f1fb5e2616fae0bdc66d74278f9071cdc61
Dat Duong <123020148+ddatba@users.noreply.github.com>
2023-06-02 10:24:03 +0100

    Update graph.py

--- graph/graph.py
+++ graph/graph.py
@@ -1,8 +1,45 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+def splt(h, n=10, m = 500, xbound = [-2,2], ybound = [-2,2], figsize= 10, ax=True, shift=False, theta=0, s=np.log(p)):
+    x = np.linspace(xbound[0],xbound[1],m)
+    y = np.linspace(ybound[0],ybound[1],m)
+    xval, yval = np.meshgrid(x,y)
+    z = xval + yval * 1j
+    f = h(z)
+    Re = f.real
+    Im = f.imag
+    Im = Im.astype(float)
 
-def splt(h, n = 16, m = 500, sing = True, a = 1, xbound = [-2,2], ybound = [-2,2], figsize = 10, ax = True):
+    Im = np.where(Re<=1, Im, np.nan)
+    Im = np.where(Re>=0, Im, np.nan)
+
+    if shift:
+        if -np.pi<theta<np.pi:
+            if theta>0:
+                Im = np.where(Im>=theta/s, Im-2*np.pi/np.abs(s), Im)
+            else:
+                Im = np.where(Im<=theta/s, Im+2*np.pi/np.abs(s), Im)
+        else:
+            raise Exception('invalid theta')
+
+    imax = np.nanmax(Im)
+    imin = np.nanmin(Im)
+    b = (imax-imin)/(2*n)
+    
+    Im = np.where(Im<imax-b/4, Im, np.nan)
+    Im = np.where(Im>imin+b/4, Im, np.nan)
+
+    plt.figure(figsize=[figsize,figsize])
+    plt.contour(xval, yval, Im, np.linspace(imin+b,imax-b,n), colors='black', linestyles='solid')
+    plt.contour(xval, yval, Re, [0,1], colors=['blue', 'red'])
+    plt.axis('scaled')
+    plt.axis(xbound+ybound)
+    if not ax:
+        plt.axis('off')
+
+
+def splt2(h, n = 16, m = 500, sing = True, a = 1, xbound = [-2,2], ybound = [-2,2], figsize = 10, ax = True):
     x = np.linspace(xbound[0], xbound[1], m)
     y = np.linspace(ybound[0], ybound[1], m)
     xval, yval = np.meshgrid(x, y)
@@ -24,33 +61,5 @@ def splt(h, n = 16, m = 500, sing = True, a = 1, xbound = [-2,2], ybound = [-2,2
     plt.contour(xval, yval, Re, [0,1], colors=['blue', 'red'])
     plt.axis('scaled')
     plt.axis(xbound + ybound)
-    if not ax:
-        plt.axis('off')
-        
-def splt2(h, n=10, m = 500, xbound = [-2,2], ybound = [-2,2], figsize= 10, ax=True):
-    x = np.linspace(xbound[0],xbound[1],m)
-    y = np.linspace(ybound[0],ybound[1],m)
-    xval, yval = np.meshgrid(x,y)
-    z = xval + yval * 1j
-    f = h(z)
-    Re = f.real
-    Im = f.imag
-    Im = Im.astype(float)
-
-    Im = np.where(Re<=1, Im, np.nan)
-    Im = np.where(Re>=0, Im, np.nan)
-
-    imax = np.nanmax(Im)
-    imin = np.nanmin(Im)
-    b = (imax-imin)/(2*n)
-    
-    Im = np.where(Im<imax-b/4, Im, np.nan)
-    Im = np.where(Im>imin+b/4, Im, np.nan)
-
-    plt.figure(figsize=[figsize,figsize])
-    plt.contour(xval, yval, Im, np.linspace(imin+b,imax-b,n), colors='black', linestyles='solid')
-    plt.contour(xval, yval, Re, [0,1], colors=['blue', 'red'])
-    plt.axis('scaled')
-    plt.axis(xbound+ybound)
     if not ax:
         plt.axis('off')
