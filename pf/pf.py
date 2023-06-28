7a4ffb9de90aa15b18b9d3ebbe4d1e95d45755ad
Dat Duong <123020148+ddatba@users.noreply.github.com>
2023-06-28 20:03:23 +0100

    Update pf.py

--- pf/pf.py
+++ pf/pf.py
@@ -2,17 +2,19 @@ import dual as d
 import numpy as np
 
 
-def C(p, eps=1e-10):  #C-value in wA
+def C(p, eps=1e-10, t=100):  #C-value in wA
     '''
         Approximates C in wA. 
         
             Parameter: 
                 p : p-value (s) 
                 *eps (float): Desired error bound 
+                *t (int):  Max number of iteration
 
             Return: 
                 estimate (float/dual/array): Estimate of C at p with the same type as p 
     '''
+    m=t
     if np.all(isinstance(p, Dual)):
         q = p.real
     else:
@@ -20,14 +22,18 @@ def C(p, eps=1e-10):  #C-value in wA
     if np.all(np.abs(q)<1):
         n=2
         c = (1-p**2)**2
-        while p**(2*n)>=eps:
+        while p**(2*n)>=eps and m>0:
             c *= (1-p**(2*n))**2
-            n +=1
-        return c*(1-p**(2*n))**2
+            n += 1
+            m -= 1
+        if m>0: 
+            return c*(1-p**(2*n))**2
+        else:
+            raise Exception('Max iteration for C reached')
     else:
         raise Exception('invalid p')
 
-def P(z,p, eps=1e-10):  #P-value in wA
+def P(z,p, eps=1e-10, t=100):  #P-value in wA
     '''
     Approximates P in wA. 
 
@@ -35,10 +41,12 @@ def P(z,p, eps=1e-10):  #P-value in wA
             z : z-value(s) 
             p : p-value(s) 
             *eps (float): Desired error bound 
+            *t (int):  Max number of iteration
 
         Return:  
             estimate (float/dual/array): Estimate of P at z, p with the same type as z and p 
     '''
+    m = t
     if np.all(isinstance(z, Dual)):
         Z = z.real
     else:
@@ -57,14 +65,18 @@ def P(z,p, eps=1e-10):  #P-value in wA
     if np.all(np.abs(q)<1):
         n = 2
         a = (1-z*p**2)*(1-(p**2)/z)
-        while np.any(np.abs((Z+1/Z)*q**(2*n)-q**(4*n))>=eps):
+        while np.any(np.abs((Z+1/Z)*q**(2*n)-q**(4*n))>=eps) and m>0:
             a *= (1-z*p**(2*n))*(1-(p**(2*n))/z)
-            n +=1
-        return (1-z)*a*(1-z*p**(2*n))*(1-(p**(2*n))/z)
+            n += 1
+            m -= 1
+        if m>0: 
+            return (1-z)*a*(1-z*p**(2*n))*(1-(p**(2*n))/z)
+        else:
+            raise Exception('Max iteration for P reached')
     else:
         raise Exception('invalid p')
     
-def wA(z,a,p, eps=1e-10):  #prime function of annulus
+def wA(z,a,p, eps=1e-10, t=100):  #prime function of annulus
     '''
     Approximates wA. 
 
@@ -73,13 +85,27 @@ def wA(z,a,p, eps=1e-10):  #prime function of annulus
             a : a-value(s) 
             p : p-value(s) 
             *eps (float): Desired error bound 
+            *t (int):  Max number of iteration
 
         Return:  
             estimate (float/dual/array): Estimate of P at z, a, p with the same type as z, a and p 
     '''
-    return -a*P(z/a,p,eps**2)/C(p, eps**2)
+    return -a*P(z/a, p, eps**2, t=t)/C(p, eps**2, t=t)
     
-def K(z, a, p, eps=1e-10, t=1000):
+def K(z, a, p, eps=1e-10, t=1000): #partial derivative of wA w.r.t z
+    '''
+        Approximates K. 
+        
+            Parameter: 
+                z : z-value(s) 
+                a : a-value(s) 
+                p : p-value (s) 
+                *eps (float): Desired error bound 
+                *t (int):  Max number of iteration
+
+            Return: 
+                estimate (float/dual/array): Estimate of K
+    '''
     m = t
     if isinstance(z, Dual):
         Z = z.real
@@ -105,5 +131,5 @@ def K(z, a, p, eps=1e-10, t=1000):
     if m > 0:
         return b + (p ** (2 * n)) * (-z / a + a / z) / (1 - (p ** (2 * n)) * (z / a + a / z - (p ** (2 * n))))
     else:
-        raise Exception("Max iteration reached")
+        raise Exception("Max iteration for K reached")
 
