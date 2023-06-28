d02479d1e2dd6b3ec07b48c6eda2c883665a9422
Dat Duong <123020148+ddatba@users.noreply.github.com>
2023-06-28 19:29:59 +0100

    Update pf.py

--- pf/pf.py
+++ pf/pf.py
@@ -3,6 +3,16 @@ import numpy as np
 
 
 def C(p, eps=1e-10):  #C-value in wA
+    '''
+        Approximates C in wA. 
+        
+            Parameter: 
+                p : p-value (s) 
+                *eps (float): Desired error bound 
+
+            Return: 
+                estimate (float/dual/array): Estimate of C at p with the same type as p 
+    '''
     if np.all(isinstance(p, Dual)):
         q = p.real
     else:
@@ -13,36 +23,58 @@ def C(p, eps=1e-10):  #C-value in wA
         while p**(2*n)>=eps:
             c *= (1-p**(2*n))**2
             n +=1
-        return c
+        return c*(1-p**(2*n))**2
     else:
         raise Exception('invalid p')
 
 def P(z,p, eps=1e-10):  #P-value in wA
-    zval = z
+    '''
+    Approximates P in wA. 
+
+        Parameters: 
+            z : z-value(s) 
+            p : p-value(s) 
+            *eps (float): Desired error bound 
+
+        Return:  
+            estimate (float/dual/array): Estimate of P at z, p with the same type as z and p 
+    '''
     if np.all(isinstance(z, Dual)):
-        Z = zval.real
+        Z = z.real
     else:
-        Z = zval
+        Z = z
     if np.all(isinstance(p, Dual)):
         q = p.real
     else:
         q = p
     if np.any(Z==0):
-        if type(Z,np.ndarray):
+        if np.all(isinstance(Z,np.ndarray)):
             raise Exception('z=0 unsupported for array')  
         elif q==0:
-            return 1
+            return 1 - z
         else:
             raise Exception('z=0 is invalid') 
     if np.all(np.abs(q)<1):
         n = 2
-        a = (1-zval*p**2)*(1-(p**2)/zval)
+        a = (1-z*p**2)*(1-(p**2)/z)
         while np.any(np.abs((Z+1/Z)*q**(2*n)-q**(4*n))>=eps):
-            a *= (1-zval*p**(2*n))*(1-(p**(2*n))/zval)
+            a *= (1-z*p**(2*n))*(1-(p**(2*n))/z)
             n +=1
-        return (1-z)*a
+        return (1-z)*a*(1-z*p**(2*n))*(1-(p**(2*n))/z)
     else:
         raise Exception('invalid p')
     
 def wA(z,a,p, eps=1e-10):  #prime function of annulus
+    '''
+    Approximates wA. 
+
+        Parameters: 
+            z : z-value(s) 
+            a : a-value(s) 
+            p : p-value(s) 
+            *eps (float): Desired error bound 
+
+        Return:  
+            estimate (float/dual/array): Estimate of P at z, a, p with the same type as z, a and p 
+    '''
     return -a*P(z/a,p,eps**2)/C(p, eps**2)
